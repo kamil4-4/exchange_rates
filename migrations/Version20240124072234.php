09f7250ae4e3ecf848cedf648ce124cf201841e6
Kamil Marczak <kamil111kamilm@gmail.com>
2024-01-24 10:13:02 +0100

    fix migration

--- migrations/Version20240124072234.php
+++ migrations/Version20240124072234.php
@@ -21,15 +21,11 @@ final class Version20240124072234 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE currency (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, currency_code VARCHAR(10) NOT NULL, exchange_rate DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('DROP TABLE dolar');
-        $this->addSql('DROP TABLE euro');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE dolar (id INT AUTO_INCREMENT NOT NULL, date DATETIME NOT NULL, exchange_rate DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE euro (id INT AUTO_INCREMENT NOT NULL, date DATETIME NOT NULL, exchange_rate DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('DROP TABLE currency');
     }
 }
